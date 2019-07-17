75dcde1364056c089725a0dcc79ef385688643bd
Mark Pascua <markpascua1012@gmail.com>
2019-07-17 14:59:32 -0700

    fixed mrnobody

--- liri.js
+++ liri.js
@@ -1,24 +1,13 @@
 require("dotenv").config();
 var moment = require("moment");
-
 var axios = require("axios");
-
 var fs = require("fs");
-
-
 var keys = require("./keys.js");
-
 var Spotify = require('node-spotify-api');
-
 var spotify = new Spotify(keys.spotify);
 
 
-
-
 var command = process.argv[2];
-
-
-
 
 
 
@@ -27,7 +16,7 @@ function concertThis(artist) {
 
     // node liri.js concert-this <artist/band name here>
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (response) {
-        // console.log(response.data)
+        console.log(response.data)
         var time = moment(response.data[0].datetime).format('MM/DD/YYYY');
         console.log("~~~~~~~~~~~~~~~~~~")
         console.log(response.data[0].lineup[0])
@@ -50,34 +39,32 @@ function concertThis(artist) {
 
 
 function movieThis(movieName) {
+    if (!movieName) {
+        movieName = "Mr.Nobody"
+        console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/")
+        console.log("It's on Netflix")
+        return movieThis(movieName);
+    }
 
     axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(function (response) {
-        if (movieName === undefined) {
-            movieName = "Mr.Nobody"
-            console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/")
-            console.log("It's on Netflix")
-            return movieThis(movieName);
-        } else {
-            console.log("~~~~~~~~~~~~~~~~~~")
-            // * Title of the movie.
-            console.log("Title: " + response.data.Title);
-            // * Year the movie came out.
-            console.log("Year: " + response.data.Year);
-            // * IMDB Rating of the movie.
-            console.log("IMDB Rating: " + response.data.imdbRating);
-            // * Rotten Tomatoes Rating of the movie.
-            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value)
-            // * Country where the movie was produced.
-            console.log("Country: " + response.data.Country)
-            // * Language of the movie.
-            console.log("Language: " + response.data.Language)
-            // * Plot of the movie.
-            console.log("Plot: " + response.data.Plot)
-            // * Actors in the movie.
-            console.log("Actors: " + response.data.Actors)
-            console.log("~~~~~~~~~~~~~~~~~~")
-        }
-
+        console.log("~~~~~~~~~~~~~~~~~~")
+        // * Title of the movie.
+        console.log("Title: " + response.data.Title);
+        // * Year the movie came out.
+        console.log("Year: " + response.data.Year);
+        // * IMDB Rating of the movie.
+        console.log("IMDB Rating: " + response.data.imdbRating);
+        // * Rotten Tomatoes Rating of the movie.
+        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value)
+        // * Country where the movie was produced.
+        console.log("Country: " + response.data.Country)
+        // * Language of the movie.
+        console.log("Language: " + response.data.Language)
+        // * Plot of the movie.
+        console.log("Plot: " + response.data.Plot)
+        // * Actors in the movie.
+        console.log("Actors: " + response.data.Actors)
+        console.log("~~~~~~~~~~~~~~~~~~")
     })
 }
 
@@ -87,10 +74,7 @@ function spotifyThis(songName) {
     spotify
         .search({ type: 'track', query: songName, limit: 1, })
         .then(function (response) {
-            if (songName === undefined) {
-                songName = "The Sign ace"
-                return spotifyThis(songName)
-            } else {
+    
                 var spot = response.tracks.items[0]
                 // console.log(spot)
                 console.log("~~~~~~~~~~~~~~~~~~")
@@ -99,11 +83,12 @@ function spotifyThis(songName) {
                 console.log("Preview Link: " + spot.preview_url)
                 console.log("Album: " + spot.album.name)
                 console.log("~~~~~~~~~~~~~~~~~~")
-            }
+            
 
         })
         .catch(function (err) {
             console.log(err);
+            
         });
 
 }
@@ -120,7 +105,7 @@ function spotifyThis(songName) {
 function doThis() {
     fs.readFile("random.txt", "utf8", function (error, data) {
 
-        
+
         if (error) {
             return console.log(error);
         }
@@ -134,7 +119,7 @@ function doThis() {
 
         spotifyThis(name);
 
-        
+
         console.log(dataArr);
 
     });
@@ -142,14 +127,14 @@ function doThis() {
 }
 
 function addThis() {
-    fs.appendFile("random.txt",", "  + "spotify-this-song," + "'" + process.argv[3] + "'", function (err) {
+    fs.appendFile("random.txt", ", " + "spotify-this-song," + "'" + process.argv[3] + "'", function (err) {
 
-        
+
         if (err) {
             return console.log(err);
         }
 
-        
+
         console.log("random.txt was updated!");
 
     });
@@ -164,11 +149,14 @@ function runcommand(command, data) {
     }
     if (command === "spotify-this-song") {
         spotifyThis(data)
+        if (!songName) {
+            songName = "The Sign ace"
+        }
     }
     if (command === "do-what-it-says") {
         doThis()
     }
-    if (command === "add-this"){
+    if (command === "add-this") {
         addThis()
     }
 
